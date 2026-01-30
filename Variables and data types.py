9791b5c000f9cbd86d2e147b4745d539ca08d763
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-01-30 18:49:58 +0530

    Create Variables and data types.py

---
+++ Variables and data types.py
@@ -0,0 +1,9 @@
+name="Eshwari"          #str
+age=18                  #int
+height=5.6              #float
+is_student=True         #boolean
+
+print(name)
+print(age)
+print(height)
+print(is_student)
