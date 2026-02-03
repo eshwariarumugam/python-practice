fee18ff2edf57f5f023742224baa3023a117aa23
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-03 18:53:55 +0530

    Create Greater_of_two_number.py

---
+++ Greater_of_two_number.py
@@ -0,0 +1,10 @@
+#Greater of two numbers
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+if a>b:
+    print( a,"is greater than" ,b)
+elif b>a:
+    print(b,"is greater than" ,a)
+else:
+    print ("Both are equal")
+    
