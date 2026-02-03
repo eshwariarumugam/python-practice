2e11a37f7a5f75822bffde7f6a8f39f5c29e1e3a
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-03 18:57:17 +0530

    Create Eligibility _for exam.py

---
+++ Eligibility _for exam.py
@@ -0,0 +1,6 @@
+#check eligibility for exam
+attendance_percentage=int(input("Enter your attendance percentage:"))
+if  attendance_percentage>=75:
+    print("You are eligible for exam")
+else:
+    print("Not eligible for exam")
