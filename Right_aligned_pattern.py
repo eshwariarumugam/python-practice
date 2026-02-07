c8a01f7c5cbcbbc47041490f619dfe326d2b99c3
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-07 21:23:59 +0530

    Create Right_aligned_pattern.py

---
+++ Right_aligned_pattern.py
@@ -0,0 +1,8 @@
+#  right aligned star printed  
+rows=int(input("Enter the number of rows:"))
+for i in range(1,rows+1):
+    for space in range(rows-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print( ) 
