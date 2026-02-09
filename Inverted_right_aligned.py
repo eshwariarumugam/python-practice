bd92afa253a2998ee8e1f9032849486b5fb128aa
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-09 20:23:13 +0530

    Create Inverted_right_aligned.py

---
+++ Inverted_right_aligned.py
@@ -0,0 +1,8 @@
+#inverted right aligned 
+rows=int(input("Enter the number of rows:"))  
+for i in range(rows,0,-1):
+    for space in range(rows-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()
