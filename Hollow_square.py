2bda31a94fcfd207b60d82370ae77e5d903dce93
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-10 20:54:47 +0530

    Create Hollow_square.py

---
+++ Hollow_square.py
@@ -0,0 +1,9 @@
+#Hollow square
+rows=int(input("Enter rows: "))
+for i in range(rows):
+    for j in range(rows):
+        if i==0 or j==0 or i==rows-1 or j==rows-1:
+            print("*",end="")
+        else:
+            print(" ",end="")    
+    print()      
