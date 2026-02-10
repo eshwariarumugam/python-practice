6e23f669e7c2a3e42f969b3811e0d5bbcb894899
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-10 21:07:18 +0530

    Create Hollow_pyramid.py

---
+++ Hollow_pyramid.py
@@ -0,0 +1,12 @@
+#Hollow pyramid   
+rows=int(input("Enter rows: "))
+for i in range(1,rows+1):
+    for space in range(rows-i):
+        print(" ",end=" ")
+    for j in range(1,2*i):
+        if j==1 or j==2*i-1 or i==rows:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")    
+    print()     
+      
